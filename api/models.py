e13b107ff0cde60a7b06100a35610fd994333169
mgomez05 <gomezruss7@gmail.com>
2022-10-08 18:56:24 +0200

    Added the model ShortUrl to models.py, and made a migration for it

--- api/models.py
+++ api/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.db.models import Model
 
 # Create your models here.
+class ShortUrl(Model):
+    realUrl = models.CharField(max_length=200)
+    shortUrl = models.URLField()
