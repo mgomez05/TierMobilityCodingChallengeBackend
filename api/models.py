a7626b8f8d25e176a79b42271bb8df0754bc0433
mgomez05 <gomezruss7@gmail.com>
2022-10-10 11:29:41 +0200

    Changed type of the ShortUrl's shortUrl field to be a CharField

--- api/models.py
+++ api/models.py
@@ -6,4 +6,4 @@ SHORT_URL_DOMAIN = "https://tier.app/"
 # Create your models here.
 class ShortUrl(Model):
     realUrl = models.CharField(max_length=200)
-    shortUrl = models.URLField()
+    shortUrl = models.CharField(max_length=200)
